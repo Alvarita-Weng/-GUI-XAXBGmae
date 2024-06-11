6e707d7d3ff374fb3872b1698fbf28484429f57e
翁欣惠 <s1101288@gm.pu.edu.tw>
2024-06-12 00:49:41 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace 視窗版本的XAXB_遊戲
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.Results = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(81, 110);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 29);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(76, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "請輸入三個不重複數字(0~9)";
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Font = new System.Drawing.Font("Stencil", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.Location = new System.Drawing.Point(271, 99);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(118, 45);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "GUESS";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Results
+            // 
+            this.Results.FormattingEnabled = true;
+            this.Results.ItemHeight = 18;
+            this.Results.Location = new System.Drawing.Point(271, 171);
+            this.Results.Name = "Results";
+            this.Results.Size = new System.Drawing.Size(330, 202);
+            this.Results.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Results);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.ListBox Results;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Reflection.Emit;
+using System.Windows.Forms;
+
+namespace 視窗版本的XAXB_遊戲
+{
+    public partial class Form1 : Form
+    {
+        private XaXbEngine xaxb; //xaxb遊戲引擎
+        private int counter;     //猜測次數計時器
+
+        public Form1()  //Form1的建構函式
+        {
+            InitializeComponent();
+            xaxb = new XaXbEngine(); //初始化xaxb遊戲引擎
+            counter = 0;             //初始化猜測次數
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string GuessNum = textBox1.Text.Trim(); // 使用者輸入的數字
+            if (xaxb.IsLegal(GuessNum)) // 檢查輸入的數字是否合法
+            {
+                counter++; // 猜測次數加一
+                string result = xaxb.GetResult(GuessNum); // 獲取猜測結果
+                Results.Items.Add($"{GuessNum}: {result}， 猜測次數: {counter}"); // 將猜測結果顯示在列表框中
+                if (result == "3A0B") // 如果猜對了
+                {
+                    Results.Items.Add("恭喜你，猜對了!"); // 顯示恭喜訊息
+                    btnGuess.Enabled = false; // 禁用猜測按鈕
+                }
+            }
+            else
+            {
+                MessageBox.Show("輸入的資料不對，或長度不對!!請重新輸入!!"); // 提示用戶重新輸入
+            }
+            textBox1.Clear(); // 清空輸入框
+        }
+    }
+
+    public class XaXbEngine   //XaXb引擎類別
+    {
+        string luckynum;      //幸運數字
+        public XaXbEngine()   //隨機生成幸運數字(建構函式)
+        {
+            Random random = new Random();    
+            int[] tem = new int[3];
+            tem[0] = random.Next(0, 9);
+            tem[1] = random.Next(0, 9);
+            tem[2] = random.Next(0, 9);
+            while (tem[0] == tem[1] ^ tem[1] == tem[2] ^ tem[0] == tem[2])
+            {
+                tem[1] = random.Next(0, 9);
+                tem[2] = random.Next(0, 9);
+            }
+            luckynum = $"{tem[0]}{tem[1]}{tem[2]}";
+        }
+       
+        public Boolean IsLegal(String theNumber)   //檢查數字是否合法
+        {
+            char[] tem = theNumber.ToCharArray();
+            if (tem.Length == 3)
+            {
+                if (tem[0] != tem[1] ^ tem[1] != tem[2] ^ tem[0] != tem[2])
+                {
+                    return true;   //數字不重複--> 合法
+                }
+                else
+                {
+                    return false;  //數字重複--> 不合法
+                }
+            }
+            else
+            {
+                return false;      //數字長度!=3 --> 不合法
+            }
+        }
+        
+        //幸運數字與猜測數字的比對結果
+        public string GetResult(String GuessNum)   //GuessNum和luckynum每個數字配對
+        {
+            char[] user = GuessNum.ToCharArray();
+            char[] ans = this.luckynum.ToCharArray();
+            int a = 0;
+            int b = 0;
+            for (int i = 0; i < user.Length; i++)
+            {
+                for (int j = 0; j < ans.Length; j++)
+                {
+                    if (user[i] == ans[j])
+                    {
+                        if (i == j)
+                        {
+                            a++;    //猜中數字且位置相同 --> A加一
+                        }
+                        else
+                        {
+                            b++;    //猜中數字但位置不同 --> B加一
+                        }
+                    }
+                }
+            }
+            return $"{a}A{b}B";
+        }
+        public Boolean IsGameover(String GuessNum) //判斷遊戲是否要結束
+        {
+            return GetResult(GuessNum) == "3A0B"; // 判斷是否猜中全部數字和位置
+        }
+    }
+
+}
